102dac0876bf22d2fe1aaec65899204c0582964a
Lukas Prediger <lukas.m.prediger@aalto.fi>
2021-11-09 11:55:38 +0200

    Added chacha.native.cuda_supported

--- lib/python_bindings.cpp
+++ lib/python_bindings.cpp
@@ -5,6 +5,15 @@
 
 #include "chacha_kernels.hpp"
 
+constexpr bool cuda_supported()
+{
+#ifdef CUDA_ENABLED
+    return true;
+#else
+    return false;
+#endif
+}
+
 PYBIND11_MODULE(native, m)
 {
     m.def("cpu_chacha20_block_factory",
@@ -14,4 +23,6 @@ PYBIND11_MODULE(native, m)
     m.def("gpu_chacha20_block_factory",
           []() { return pybind11::capsule(reinterpret_cast<void*>(gpu_chacha20_block), "xla._CUSTOM_CALL_TARGET"); });
 #endif // CUDA_ENABLED
+
+    m.def("cuda_supported", &cuda_supported, "Returns true if CUDA kernels were compiled.");
 }
