bf04408d7ef3a8230c3422f7b8ce6c02608fb80e
Lukas Prediger <lukas.m.prediger@aalto.fi>
2023-03-29 14:44:49 +0300

    fixing missing definition for gpuStream_t

--- lib/chacha_kernels.hpp
+++ lib/chacha_kernels.hpp
@@ -3,11 +3,20 @@
 
 #pragma once
 
+
 // cpu_kernel.cpp
 extern void cpu_chacha20_block(void* out_buffer, const void** in_buffers);
 
 #if (CUDA_ENABLED || HIP_ENABLED)
-    class gpuStream_t;
+    #ifdef CUDA_ENABLED
+        #include <cuda_runtime.h>
+        typedef cudaStream_t gpuStream_t;
+    #endif // CUDA_ENABLED
+
+    #ifdef HIP_ENABLED
+        #include <hip/hip_runtime.h>
+        typedef hipStream_t gpuStream_t;
+    #endif // HIP_ENABLED
 
     // gpu_kernel.cpp.cu
     extern void gpu_chacha20_block(gpuStream_t stream, void** buffers, const char* opaque, std::size_t opaque_length);
